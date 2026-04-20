601ee067ba7edf535c9194a8fda8de196f510051
NaveenVicky770 <naveenvicky770@gmail.com>
2026-04-20 10:17:53 +0000

    change regsitry url for ps1

--- cmd/worker/k8s.go
+++ cmd/worker/k8s.go
@@ -347,7 +347,7 @@ func (w *worker) CreateNotebook() error {
 
 	initContainers := []any{}
 
-	if imageName != "098809772313.dkr.ecr.ap-south-1.amazonaws.com/tgdex/ai-sandbox-cpu-notebook:nha-ps1" {
+	if imageName != "098809772313.dkr.ecr.ap-south-1.amazonaws.com/tgdex/ai-sandbox-cpu-notebook:nha-ps1-v1" {
 		initContainers = append(initContainers, map[string]any{
 			"name":  "init-demo-ipynb",
 			"image": w.app.env.INIT_CONTAINER_IMAGE,
