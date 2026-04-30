fcb635182dc779c612a2020aa65890913d3ee90e
NaveenVicky770 <naveenvicky770@gmail.com>
2026-04-30 10:08:56 +0000

    User docs and tutorials build and serve

--- user-docs/docusaurus.config.js
+++ user-docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   organizationName: 'datakaveri',
   projectName: 'sandbox-connect-api',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   i18n: {
